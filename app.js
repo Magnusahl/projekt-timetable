6dc9911cff884b7375162e9c3104cf96c6a6824c
Magnus <51696488+Magnusahl@users.noreply.github.com>
2019-11-09 20:57:44 +0100

    coding

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-fetch {
-    URL: https: //api.sl.se/api2/realtimedeparturesV4.<FORMAT>?key=<3b1f16fa9f144aa1aad3e9d76b06cbbe>&siteid=<SITEID>&timewindow=<TIMEWINDOW>
-
-}
+fetch(
+        "https://cors-anywhere.herokuapp.com/https://api.sl.se/api2/realtimedeparturesV4.<FORMAT>?key=<3b1f16fa9f144aa1aad3e9d76b06cbbe>&siteid=<SITEID>&timewindow=<TIMEWINDOW>"
+    )
+    .then(res => res.json())
+    .then(data => console.log(data));
