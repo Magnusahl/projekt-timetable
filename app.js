41e71a47b61da5004e5d0427aa4bc0fe3a6cb167
Magnus Ahlqvist <51696488+Magnusahl@users.noreply.github.com>
2019-11-11 11:33:00 +0100

    working code

--- app.js
+++ app.js
@@ -1,5 +1,44 @@
-fetch(
-        "https://cors-anywhere.herokuapp.com/https://api.sl.se/api2/realtimedeparturesV4.<FORMAT>?key=<3b1f16fa9f144aa1aad3e9d76b06cbbe>&siteid=<SITEID>&timewindow=<TIMEWINDOW>"
-    )
-    .then(res => res.json())
-    .then(data => console.log(data));
+var station;
+var siteId;
+
+function myPlats(){
+    const url = "https://cors-anywhere.herokuapp.com/https://api.sl.se/api2/typeahead.json?key=93b313c352944b7590052248617514e0&searchstring="+station+"&stationsonly=True&maxresults=1";
+    fetch(url)
+      .then((resp) => resp.json())
+      .then(function (data) {
+        let infos = data.ResponseData;
+        return infos.map(function (info) {
+            station = document.getElementById("resa").value;
+            siteId = info.SiteId;
+            innerHTML = "";
+            console.log("1");
+            myAvgang(siteId);
+        })
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+}
+
+    
+function myAvgang(siteId) {
+    const span = document.getElementById('info');
+    const url = "https://cors-anywhere.herokuapp.com/https://api.sl.se/api2/realtimedeparturesV4.json?key=3b1f16fa9f144aa1aad3e9d76b06cbbe&siteid=" + siteId + "&timewindow=60";
+    fetch(url)
+      .then((resp) => resp.json())
+      .then(function (data) {
+        let infos = data.ResponseData.Trams;
+        return infos.map(function (info) {
+            console.log("2");
+          span.innerHTML += info.Destination + " " + info.DisplayTime +"<br>";
+        })
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+}
+
+
+
+
+
